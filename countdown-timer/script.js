465aa686b9a37a22678826c301eafbba808b6fcd
Unnati <unnatikolhe@gmail.com>
2021-02-13 18:59:14 +0530

    completed countdown_timer

---
+++ countdown-timer/script.js
@@ -0,0 +1,20 @@
+const mainDay = new Date("1 Sep 2021").getTime();
+
+var x = setInterval(() => {
+  var now = new Date().getTime();
+  var distance = mainDay - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
